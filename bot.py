241cb78ee19eb56a19a1fd924123b9b9c47a3df5
Kirillchik <kirlir711@gmail.com>
2024-12-19 21:04:20 +0200

    command /start

--- bot.py
+++ bot.py
@@ -2,16 +2,37 @@ import aiogram
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 from config import TOKEN
-
 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+####### Feader
 
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer(f'Вітаю, {message.from_user.username}! Це Бот, який допоможе тобі стати кращою версією тебе)')
+    await message.answer('Згодом тут буде меню де можна вибрати основні функції бота. Зокрема, інформація про можливості бота, майбутні зміни і далі головні команди.')
+
+
+
+
+
+
+
+
+
+
+
+######### Footer
 async def main():
     await dp.start_polling(bot)
-
+    
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Bot stoped')
