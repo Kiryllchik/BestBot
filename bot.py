1b94d9fb2f164a858ce48ce9f304ecb1a9819ceb
Kirillchik <kirlir711@gmail.com>
2024-12-20 20:29:48 +0200

    crated the class User. Created command /user_info

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import aiogram 
 import asyncio
+from models.user import User
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from config import TOKEN
 
@@ -15,7 +16,14 @@ dp = Dispatcher()
 async def cmd_start(message: Message):
     await message.answer(f'Вітаю, {message.from_user.username}! Це Бот, який допоможе тобі стати кращою версією тебе)')
     await message.answer('Згодом тут буде меню де можна вибрати основні функції бота. Зокрема, інформація про можливості бота, майбутні зміни і далі головні команди.')
+    
+    usr = message.from_user
+    global new_user
+    new_user = User(id=usr.id, name=usr.username, goals=None, reminders=None, status="free", diary=None)
 
+@dp.message(Command('user_info'))
+async def user_info(message: Message):
+    await message.reply(f'ID: {new_user.id}\nName: {new_user.name}\nStatus: {new_user.status}')
 
 
 
---
+++ models/user.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__ (self, id, name, goals, reminders, status, diary):
+        self.id = id
+        self.name = name
+        self.goals = goals
+        self.reminders = reminders
+        self.status = status
+        self.diary = diary
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
